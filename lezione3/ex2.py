5117557f26919148dcb8bb62befd465d5cfe8893
mauro <mauro.casadei@soluzionesoftware.com>
2022-10-10 12:42:18 +0200

    lezione 3 _ ex2

---
+++ lezione3/ex2.py
@@ -0,0 +1,8 @@
+# gestire i presenti nel campo di una partita di Calcio
+# all'inizio partita entrano 11 giocatori (creare una lista di 11 nomi)
+# al 35" minuto esce 1 giocatore e ne entra un altro, quindi sostituirlo
+# al 50" minuto c'è un infortunio e entrano i 2 massaggiatori
+# al 52" minuto escono i 2 massaggiatori
+# al 80" minuto viene espulso uno dei giocatori
+# gestire le presenze nel campo con una lista e stampare nei diversi minuti
+# il numero dei giocatori e le persone nel campo
