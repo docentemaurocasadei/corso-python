2e54153442b50bf18798a8e9f4de92098242f098
mauro <mauro.casadei@soluzionesoftware.com>
2022-10-18 09:32:45 +0200

    lezione 4

--- work/test.py
+++ work/test.py
@@ -1,12 +1,4 @@
-x = 10
-def myf(v, *args, **kwargs):
-    y = 5
-    x = 9
-    print("x è {}".format(x))
-    def myf2():
-        print("y è {}".format(y))
-        print(v, args, kwargs)
-    myf2()
-
-myf('aa', 'bb', 'cc', miav=1)
-print(x)
+d = {'nome': 'aaaa', 'cognome': 'bbbb'}
+d = dict(nome='aaa', cognome='bbb')
+for itm in d.items():
+    print(itm)
