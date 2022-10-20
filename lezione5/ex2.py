3b374e087bff34bad9b3fab5861b682f579036b2
mauro <mauro.casadei@soluzionesoftware.com>
2022-10-20 12:21:13 +0200

    lezione 5 esercizio 2

---
+++ lezione5/ex2.py
@@ -0,0 +1,12 @@
+# siamo in una scuola e dobbiamo tradurre un voto da numerico a formato alfanimerico
+# se 10 voto A
+# se 8 voto B
+# se 6 voto C
+# se 5 voto D
+# se 4 voto E
+# se 3 voto F
+# scriviamo più if .. elif per tale traduzione che dovrà essere salvata
+# nella variabile lettera e stampata con print
+
+voto = 8
+
