c952b02212834d665b690d0813f5c9790a9dc196
mauro <mauro.casadei@soluzionesoftware.com>
2022-10-20 09:55:59 +0200

    lezione 5

--- lezione5/lezione5.py
+++ lezione5/lezione5.py
@@ -1,4 +1,26 @@
 #   set
 s = {'marco', 'silvia', 'stefano', 'debora'}
-print(type(s), s)
-print('bbb')
+print(type(s), s, len(s))
+
+# s1 = set((1, 2, 3, 4))
+# # equivale
+# s1 = {1, 2, 3, 4}
+#
+# d = {'nome': 'Marco', 'cognome': 'Rossi'}
+# d = dict(nome='Marco', cognome='Rossi')
+# print(type(d), d)
+
+# print(s[0]) # il set non è accessibile tramite indici
+i = 0
+for elemento in s:
+    i += 1
+print(i)
+
+nome = 'marco'
+if nome in s:
+    print(f'{nome} sei iscritto!!')
+else:
+    print(f'{nome} non sei iscritto!!')
+
+# print('stiamo cercando {}'.format(nome))
+# print(f'stiamo cercando {nome}')
