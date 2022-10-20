bf8699d4127c21f7478db92e282d60880fe76a51
mauro <mauro.casadei@soluzionesoftware.com>
2022-10-20 10:29:38 +0200

    lezione 5

--- lezione5/lezione5.py
+++ lezione5/lezione5.py
@@ -16,6 +16,7 @@ for elemento in s:
     i += 1
 print(i)
 
+# nome = input('inserisci il nome:')
 nome = 'marco'
 if nome in s:
     print(f'{nome} sei iscritto!!')
@@ -24,3 +25,20 @@ else:
 
 # print('stiamo cercando {}'.format(nome))
 # print(f'stiamo cercando {nome}')
+
+s.add('maria')
+s.update({'carla', 'francesco'})
+
+s.discard('giacomo')
+valore_eliminato = s.pop()
+# s.clear()
+print(s)
+print(valore_eliminato)
+
+gara2 = {'lea', 'simona'}
+s1 = s.union(gara2)
+print(s)    # {'stefano', 'carla', 'francesco', 'debora', 'silvia', 'maria'}
+print(s1)   #{'lea', 'simona', 'stefano', 'carla', 'francesco', 'debora', 'silvia', 'maria'}
+
+s.update(gara2)
+print(s)   #{'lea', 'simona', 'stefano', 'carla', 'francesco', 'debora', 'silvia', 'maria'}
