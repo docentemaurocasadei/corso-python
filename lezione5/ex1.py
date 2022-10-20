a85a5e27f9dd0ee5b5bea42af7a8960c8fa09811
mauro <mauro.casadei@soluzionesoftware.com>
2022-10-20 11:49:43 +0200

    lezione 5 esercizio

--- lezione5/ex1.py
+++ lezione5/ex1.py
@@ -7,3 +7,17 @@
 # realizzare un loop con ciclo for
 # all'interno del loop inserire una if con operatore in
 #
+
+fiera1 = {'alfa', 'beta', 'gamma', 'google'}
+fiera2 = {'iota', 'revenge', 'gamma', 'sigma', 'google'}
+contatti_duplicati = set()
+
+for contatto in fiera1:
+    if contatto in fiera2:
+        print(f'{contatto} è duplicato')
+        contatti_duplicati.add(contatto)    # aggiunge un elemento al set
+
+print(contatti_duplicati)
+# la stessa cosa con metodo intersection
+contatti_duplicati2 = fiera1.intersection(fiera2)
+print('contatti_duplicati2:', contatti_duplicati2)
