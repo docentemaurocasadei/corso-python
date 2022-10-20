3554bb97281c6acce58dfd5bd829966290b07adb
mauro <mauro.casadei@soluzionesoftware.com>
2022-10-20 10:34:31 +0200

    lezione 5 esercizio

---
+++ lezione5/ex1.py
@@ -0,0 +1,8 @@
+# siamo una azienda che partecipa a fiere internazionali
+# durante le ultime 2 fiere sono stati acquisiti dei contatti
+# fiera1 = {'alfa', 'beta', 'gamma', 'google'}
+# fiera2 = {'iota', 'revenge', 'gamma', 'sigma', 'google'}
+# ritornati in ufficio occorre identificare quale contatto è duplicato tra le 2 fiere
+# per farlo:
+# realizzare un loop con ciclo for
+# all'iterno del loop inserire una if con operatore in#
