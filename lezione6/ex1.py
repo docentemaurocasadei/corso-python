7a228afa005846e1e5bdf9a44ec01224e294d03e
mauro <mauro.casadei@soluzionesoftware.com>
2022-10-24 09:44:45 +0200

    lezione 6 ex1

---
+++ lezione6/ex1.py
@@ -0,0 +1,5 @@
+# creare una lista contenente i numeri da 1 a 10
+# se un numero è pari stamparlo
+# se un numero è dispari tramite l'istruzione continue ritornare al controllo del ciclo
+
+l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
--- lezione6/lezione6.py
+++ lezione6/lezione6.py
@@ -4,7 +4,29 @@
 #     pass
 # print('ciao')
 
-l = ['aaa', 'bbb', 'ccc', 'ddd']
-# for
-for elemento in l:
-    print(elemento)
+# l = ['aaa', 'bbb', 'ccc', 'ddd']
+# # for
+# print("**** for ****")
+# for elemento in l:
+#     print(elemento)
+# else:
+#     print('lista terminata')
+# # while
+# print("**** while ****")
+# i = 0
+# while i < len(l):
+#     print(l[i])
+#     i += 1
+# else:
+#     print('lista terminata')
+
+classe = ['alessandro', 'ali', 'giacomo', 'sara', 'simone', 'giovanni', 'tobia', 'patrizia']
+
+for persona in classe:
+    if persona == 'sara' or persona == 'patrizia':
+        print(f'{persona} è donna')
+        continue
+    else:
+        print(f'{persona} è uomo')
+    print(f'{persona} salta')
+
