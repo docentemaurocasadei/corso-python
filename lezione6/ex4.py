337d9bb4e2b81ad3e403fff8cce4b2f3fe2b455f
mauro <mauro.casadei@soluzionesoftware.com>
2022-10-24 13:05:52 +0200

    lezione 6 ex4

--- lezione6/ex4.py
+++ lezione6/ex4.py
@@ -4,3 +4,20 @@
 # 4,5,6,3
 # 5,8,3,2
 
+def calcola2(p1,p2,p3,p4):
+    print(p1+p2+p3+p4)
+
+calcola2(4,5,6,3)
+calcola2(5,8,3,2)
+
+# secondo esempio con parametri dinamici
+def somma(*args):
+    risultato = 0
+    for numero in args:
+        risultato += numero
+    print(risultato)
+
+somma(4, 5, 6, 3)
+somma(5, 8, 3, 2)
+somma(5, 8)
+somma(5, 8, 8, 9, 3, 4)
