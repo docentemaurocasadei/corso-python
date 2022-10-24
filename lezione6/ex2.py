d8247d44bbb50cb61c0318f7d4b56af8859b5fb1
mauro <mauro.casadei@soluzionesoftware.com>
2022-10-24 11:03:20 +0200

    lezione 6 ex2

--- lezione6/ex2.py
+++ lezione6/ex2.py
@@ -2,7 +2,23 @@
 # i primi 3 classificati sono il 15, 25, 38
 # ciclare (tramite for) su tutti i partecipanti e stampare i 3 vincitori
 
+vincitori = [15, 25, 38]
+vincitori_individuati = 0
+for pettorina in range(1,51):
+    if pettorina in vincitori:
+        print(f'il {pettorina} ha vinto!!')
+        vincitori_individuati += 1
+    if vincitori_individuati > 2:
+        break
 
+vincitori = [15, 25, 38]
+podio = list()
+for pettorina in range(1,51):
+    if pettorina in vincitori:
+        print(f'il {pettorina} ha vinto!!')
+        podio.append(pettorina)
+    if len(podio) > 2:
+        break
 
 # eseguire il medesimo esercizio tramite while
 
