c146227bc9f1d9ccf2d227f30480985c79e45ea7
mauro <mauro.casadei@soluzionesoftware.com>
2022-10-26 09:27:38 +0200

    lezione 7

---
+++ lezione7/lezione7.py
@@ -0,0 +1,11 @@
+l = ['carlo', 'sara', 'francesca', 'maria', 'stefano']
+# for
+for nome in l:
+    print(f'il nome è: {nome}')
+
+#while
+print('*****')
+i = 0
+while i < len(l):
+    print(f'il nome è: {l[i]}')
+    i +=1
