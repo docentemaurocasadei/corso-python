f2580945df8abe13208e63086f387eb3ea1a1227
mauro <mauro.casadei@soluzionesoftware.com>
2022-10-31 10:15:03 +0100

    lezione 8 ex1

--- lezione8/ex1.py
+++ lezione8/ex1.py
@@ -1,1 +1,5 @@
 # https://bdap-opendata.mef.gov.it/
+# aprire il file opendata_bil.csv
+# leggere tutte le righe del file
+# sommare uno delle colonne (ad esempio OP, oppure OP Erario oppure OP Tesoreria ecc..
+# stampare a video la somma ottenuta
