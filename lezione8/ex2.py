29e3302f95d85c55e1d836b28c95de672f8abde4
mauro <mauro.casadei@soluzionesoftware.com>
2022-10-31 13:50:07 +0100

    lezione 8 ex2

--- lezione8/ex2.py
+++ lezione8/ex2.py
@@ -7,3 +7,28 @@
 # eseguire la query
 # commit
 # leggere la tabella con tutti i record e stampare nome + codice
+import mysql.connector
+try:
+    nominativo = input('inserisci il nominativo:')
+    codice = input('inserisci il codice:')
+    #creo la connessione
+    cn = mysql.connector.connect(
+        host="localhost", user="root", password="", db="personale"
+    )
+    #creo il cursore
+    cursore = cn.cursor(dictionary=True)
+    # inserisci()
+    query = "INSERT INTO timbrature (codice, nominativo) VALUES(%s, %s)"
+    # eseguo la query
+    cursore.execute(query, [codice, nominativo])
+    # commit / rollback
+    cn.commit()
+    # aggiorna()
+    query = "SELECT * FROM timbrature"
+    cursore.execute(query)
+    risultati = cursore.fetchall()
+    for r in risultati:
+        print(r['nominativo'],r['codice'])
+
+except Exception as e:
+    print(e)
