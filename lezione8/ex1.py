59cd1a5e6469c3827157f4a75d484c298ce8b55e
mauro <mauro.casadei@soluzionesoftware.com>
2022-10-31 10:45:15 +0100

    lezione 8 ex1

--- lezione8/ex1.py
+++ lezione8/ex1.py
@@ -1,5 +1,19 @@
+import locale
 # https://bdap-opendata.mef.gov.it/
 # aprire il file opendata_bil.csv
 # leggere tutte le righe del file
 # sommare uno delle colonne (ad esempio OP, oppure OP Erario oppure OP Tesoreria ecc..
 # stampare a video la somma ottenuta
+somma = 0
+with open('opendata_bil.csv', 'r') as f:
+    for r in f:
+        try:
+            l = r.split(';')
+            # print(l[14])
+            somma += float(l[14]) # OP
+        except:
+            print('non decimale')
+
+print(somma)
+locale.setlocale(locale.LC_ALL, 'it_IT.utf8')
+print("{:,f}".format(somma))
--- work/test.py
+++
@@ -1,4 +0,0 @@
-d = {'nome': 'aaaa', 'cognome': 'bbbb'}
-d = dict(nome='aaa', cognome='bbb')
-for itm in d.items():
-    print(itm)
