b6d276f38115b94d4f190304b9aa7d3cbd8cda24
mauro <mauro.casadei@soluzionesoftware.com>
2022-10-31 12:20:59 +0100

    lezione 8 ex1

--- lezione8/lezione8.py
+++ lezione8/lezione8.py
@@ -1,3 +1,4 @@
+import mysql.connector
 # import random
 # import datetime
 # funzioni
@@ -10,18 +11,34 @@
 # print(datetime.datetime.now()) #data e ora attuale
 # print(datetime.date.today()) #data di oggi
 
-with open('testi.txt', 'a', encoding='utf-8') as f:
-    f.write('oggi è il 31/10\n')
-
-with open('testi.txt', 'r', encoding='utf-8') as f:
-    for r in f:
-        print(r, end='')
+# with open('testi.txt', 'a', encoding='utf-8') as f:
+#     f.write('oggi è il 31/10\n')
+#
+# with open('testi.txt', 'r', encoding='utf-8') as f:
+#     for r in f:
+#         print(r, end='')
+#
+# try:
+#     with open('miofile2.txt', 'r', encoding='utf-8') as miof:
+#         for r in miof:
+#             print(r)
+# except:
+#     print('c\'è stato un errore!! ') #per rosso \u001b[31m
+#
+# print('fine codice!')
 
 try:
-    with open('miofile2.txt', 'r', encoding='utf-8') as miof:
-        for r in miof:
-            print(r)
-except:
-    print('c\'è stato un errore!! ') #per rosso \u001b[31m
-
-print('fine codice!')
+    #creo la connessione
+    cn = mysql.connector.connect(
+        host="localhost", user="root", password="", db="personale"
+    )
+    #creo il cursore
+    cursore = cn.cursor(dictionary=True)
+    #creo la query INSERT
+    query = "INSERT INTO timbrature (codice, nominativo) VALUES(%s, %s)"
+    #eseguo la query
+    cursore.execute(query,['cod001','Mario Rossi'])
+    #commit / rollback
+    cn.commit()
+except Exception as e:
+    print(e)
