c851b4981e11ff3cdc8e5a4b0fbbb21ba8caa3ad
mauro <mauro.casadei@soluzionesoftware.com>
2022-10-31 13:46:29 +0100

    lezione 8 ex2

---
+++ lezione8/ex2.py
@@ -0,0 +1,9 @@
+# si richiede di inserire un record richiesto dall'utente
+# richiedere tramite input il nominativo
+# richiedere tramite input il codice
+# connettersi al database
+# definire il cursore
+# creare la query di inserimento
+# eseguire la query
+# commit
+# leggere la tabella con tutti i record e stampare nome + codice
